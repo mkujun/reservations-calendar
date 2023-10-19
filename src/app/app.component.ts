f38c89b9a102ae40d0c9fb7aff9359151425d2ff
mkujun <kujundzic.marko@hotmail.com>
2023-10-19 14:05:55 +0200

    reusable function for making bookings of different units

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { BookingJune } from 'src/mocks/booking-june';
+import { Booking } from 'src/app/booking.model';
 
 @Component({
   selector: 'app-root',
@@ -25,44 +26,51 @@ export class AppComponent implements OnInit {
     console.log(this.bookingJune);
   }
 
+  createBooking(from: number, to: number, d: Booking) {
+    if (d.day >= from && d.day <= to) {
+      d.booked = true;
+      if (d.day == from) {
+        if (d.isFirstDay == false) {
+          d.isFirstDay = true;
+        } else {
+          // if it is first, now it is also the last
+          d.isLastDay = true;
+        }
+      }
+      if (d.day == to) {
+        if (d.isLastDay == false) {
+          d.isLastDay = true;
+        } else {
+          // if it is last, now it is also the first
+          d.isFirstDay = true;
+        }
+      }
+    }
+  }
+
   onSubmitPeriod() {
     const key = this.period.value.unit + this.period.value.month;
     const from = this.period.value.from;
     const to = this.period.value.to;
 
-    // TODO: add for other units
     if (key == 'N2June') {
       this.bookingJune.N2.map((d) => {
-        if (d.day >= from && d.day <= to) {
-          d.booked = true;
-
-          if (d.day == from) {
-            if (d.isFirstDay == false) {
-              d.isFirstDay = true;
-            }
-            else { // if it is first, now it is also the last
-              d.isLastDay = true;
-            }
-          }
-          if (d.day == to) {
-            if (d.isLastDay == false) {
-              d.isLastDay = true;
-            }
-            else { // if it is last, now it is also the first
-              d.isFirstDay = true;
-            }
-          }
-        }
+        this.createBooking(from, to, d);
       });
     } else if (key == 'N3June') {
       this.bookingJune.N3.map((d) => {
-        if (d.day >= from && d.day <= to) {
-          d.booked = true;
-        }
+        this.createBooking(from, to, d);
       });
-
+    } else if (key == 'N4June') {
+      this.bookingJune.N4.map((d) => {
+        this.createBooking(from, to, d);
+      });
+    } else if (key == 'N5June') {
+      this.bookingJune.N5.map((d) => {
+        this.createBooking(from, to, d);
+      });
     }
+
     console.log(this.bookingJune);
   }
-
 }
--- src/app/booking.model.ts
+++ src/app/booking.model.ts
@@ -5,8 +5,6 @@ export class Booking {
   booked: boolean;
   isFirstDay: boolean;
   isLastDay: boolean;
-  // TODO: should add information about first and last day of booking
-  // this information is required for table css split color rendering
 
   constructor(day: number, month: string, unit: string, booked: boolean, isFirstDay: boolean, isLastDay: boolean) {
     this.day = day;
