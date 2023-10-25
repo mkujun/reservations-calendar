86828ee8dfa3b00413994909305766a997638214
mkujun <kujundzic.marko@hotmail.com>
2023-10-25 13:12:03 +0200

    bookings list component initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UnitMonthComponent } from './components/unit-month/unit-month.component';
+import { BookingsListComponent } from './components/bookings-list/bookings-list.component';
 
 @NgModule({
-  declarations: [AppComponent, UnitMonthComponent],
+  declarations: [AppComponent, UnitMonthComponent, BookingsListComponent],
   imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
